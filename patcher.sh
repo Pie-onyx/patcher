8d6ad3ce4e063f62494fd21a386ff11bbaf74dfb
YumeMichi <do4suki@gmail.com>
2019-09-05 02:48:23 +0800

    Move patcher to repositories
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- patcher.sh
+++ patcher.sh
@@ -3,23 +3,8 @@
 ROOT="${PWD}"
 
 REPOSITORIES=(
-    'art'
-    'bionic'
-    'build/kati'
-    'build/make'
-    'build/soong'
     'cts'
-    'external/f2fs-tools'
-    'external/selinux'
-    'frameworks/av'
-    'frameworks/native'
-    'frameworks/opt/net/wifi'
-    'hardware/interfaces'
-    'hardware/libhardware'
     'packages/services/Telephony'
-    'system/bt'
-    'system/core'
-    'system/vold'
 )
 
 for repository in "${REPOSITORIES[@]}"; do
